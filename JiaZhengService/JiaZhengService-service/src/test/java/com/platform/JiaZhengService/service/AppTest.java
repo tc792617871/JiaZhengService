fa650ca3f9f533cec5c3e78685d6f38105ee2b4f
tc792617871 <792617871@qq.com>
2017-11-10 21:01:06 +0800

    删除多余代码

--- JiaZhengService/JiaZhengService-service/src/test/java/com/platform/JiaZhengService/service/AppTest.java
+++ JiaZhengService/JiaZhengService-service/src/test/java/com/platform/JiaZhengService/service/AppTest.java
@@ -1,21 +1,8 @@
 package com.platform.JiaZhengService.service;
 
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.FileSystemXmlApplicationContext;
-
-import com.platform.JiaZhengService.service.api.TestService;
-
 /**
  * Unit test for simple App.
  */
 public class AppTest {
 
-	private static TestService service;
-
-	public static void main(String[] args) {
-		ApplicationContext ctx = new FileSystemXmlApplicationContext(
-				new String[] { "classpath*:applicationContext.xml" });
-		service = (TestService) ctx.getBean(TestService.class);
-		service.insertUser();
-	}
 }
